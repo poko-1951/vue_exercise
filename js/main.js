581a443e20468b1beebc143782953572a8da8c5d
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-10-04 23:53:48 +0900

    [Add]v-bind

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const app = Vue.createApp({
   data: () => ({
     message: "Hello Vue.js!",
-    count: 99,
-    user: {
-      lastName: "Kurihara",
-      firstName: "Go",
-      prefecture: "Tokyo"
-    },
-    colors: ["red", "Green", "Blue"]
+    // count: 99,
+    // user: {
+    //   lastName: "Kurihara",
+    //   firstName: "Go",
+    //   prefecture: "Tokyo"
+    // },
+    // colors: ["red", "Green", "Blue"]
   }),
 });
 app.mount("#app");
