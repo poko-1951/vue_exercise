95f63dc41ed9625e4be1524bfa1857981eb95999
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-10-05 00:15:58 +0900

    [Add]v-on

--- js/main.js
+++ js/main.js
@@ -2,13 +2,21 @@ const app = Vue.createApp({
   data: () => ({
     // message: "Hello Vue.js!",
     // count: 99,
-    user: {
-      lastName: "Kurihara",
-      firstName: "Go",
-      prefecture: "Tokyo",
-    },
+    // user: {
+    //   lastName: "Kurihara",
+    //   firstName: "Go",
+    //   prefecture: "Tokyo",
+    // },
     // colors: ["Red", "Green", "Blue"]
     // toggle: true
+    now: "-"
   }),
+  methods: {
+    onClick: function() {
+      // console.log("onClick")
+      this.now = new Date().toLocaleString()
+      console.log(this)
+    }
+  }
 });
 app.mount("#app");
